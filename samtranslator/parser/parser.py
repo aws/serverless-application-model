bb68bb7f8995e5e1efcac949de2880101378d9ea
Brett Andrews <brett.j.andrews@gmail.com>
2018-04-20 11:06:46 -0700

    fix: remove redundant macro resolver logic in parser to prevent plugins from running twice
    Eventually we want to refactor so that the parser validates everything including plugins, however that requires a larger refactor of the plugin system and macro resolver.

--- samtranslator/parser/parser.py
+++ samtranslator/parser/parser.py
@@ -10,21 +10,7 @@ class Parser:
     def parse(self, sam_template, parameter_values, sam_plugins):
         self._validate(sam_template, parameter_values)
         sam_plugins.act(LifeCycleEvents.before_transform_template, sam_template)
-        macro_resolver = ResourceTypeResolver(sam_resources)
-        document_errors = []
 
-        for logical_id, resource in sam_template["Resources"].items():
-            try:
-                if macro_resolver.can_resolve(resource):
-                    macro_resolver\
-                        .resolve_resource_type(resource)\
-                        .from_dict(logical_id, resource, sam_plugins=sam_plugins)
-            except (InvalidResourceException, InvalidEventException) as e:
-                document_errors.append(e)
-
-        if len(document_errors) > 0:
-            raise InvalidDocumentException(document_errors)
-            
     # private methods
     def _validate(self, sam_template, parameter_values):
         """ Validates the template and parameter values and raises exceptions if there's an issue
