40abf829dd63142a6480aefe705b4720c5237af8
Bruce Edge <brucee.edge@gmail.com>
2018-05-22 12:31:10 -0700

    Address requested changes.

--- examples/apps/microservice-http-endpoint-python3/test.sh
+++ examples/apps/microservice-http-endpoint-python3/test.sh
@@ -5,7 +5,7 @@
 # Args: $1: API GW path
 #       $2 json payload data file
 #
-# eg: ./test.sh https://vxldx8ck9i.execute-api.us-west-2.amazonaws.com/Prod/MyResource ./test-payload.json
+# eg: ./test.sh https://<API_ID>.execute-api.<YOUR_REGION>.amazonaws.com/Prod/MyResource ./test-payload.json
 
 set -x
 
