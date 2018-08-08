e14bc68ad946869d20619b79754f3ff889029be6
Brett Andrews <brett.j.andrews@gmail.com>
2018-08-08 15:17:36 -0700

    feat: hard-code list of managed policies and use that by default
    This was the only AWS Service call in SAM. By removing this it allows for the SAM transformer to be run offline.
    
    Maintained the initial code with an option so that in the future we an expose a way to grab the authoritative list from IAM. This also allows us to easily grab an updated list periodically.

--- tests/translator/test_managed_policies_translator.py
+++ tests/translator/test_managed_policies_translator.py
@@ -7,6 +7,22 @@ def create_page(policies):
         'Policies': map(lambda x: {'PolicyName': x[0], 'Arn': x[1]}, policies)
     }
 
+
+def test_load_local():
+    paginator = MagicMock()
+    iam = MagicMock()
+    iam.get_paginator.return_value = paginator
+
+    actual = ManagedPolicyLoader(iam).load()
+
+    assert not iam.get_paginator.called
+    assert isinstance(actual, dict)
+    assert actual.get('AmazonMechanicalTurkCrowdFullAccess')
+
+    # NOTE: this is a moving target that must be updated when ManagedPolicyLoader._local_managed_policies_map is updated
+    assert len(actual) == 401
+
+
 def test_load():
     paginator = MagicMock()
     paginator.paginate.return_value = [
@@ -27,11 +43,10 @@ def test_load():
         ]),
     ]
 
-
     iam = MagicMock()
     iam.get_paginator.return_value = paginator
 
-    actual = ManagedPolicyLoader(iam).load()
+    actual = ManagedPolicyLoader(iam).load(use_local_managed_policies=False)
     expected = {
         'Policy-1': 'Arn-1',
         'Policy-2': 'Arn-2',
