1f3d169316c49bc3aa55069162041a452f14e35f
Heitor Lessa <lessa@amazon.com>
2018-08-16 23:14:17 +0300

    fix: addresses feedback of InlineCode feature

--- examples/2016-10-31/hello_world_vpc/src/index.js
+++ examples/2016-10-31/hello_world_vpc/src/index.js
@@ -1,6 +1,1 @@
-'use strict';
-console.log('Loading function');
-
-exports.handler = (event, context, callback) => {
-    callback(null, 'Hello World!');
-};
+exports.handler = async () => 'Hello world!'
