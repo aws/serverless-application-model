9c9eda00e9dc203f432c1b190ca68acdbe7dfc89
Tony Sellitti <asellitti@acm.org>
2019-02-20 09:50:10 -0500

    Fix PR Review items and add a further test

--- samtranslator/model/sam_resources.py
+++ samtranslator/model/sam_resources.py
@@ -604,9 +604,7 @@ class SamLayerVersion(SamResourceMacro):
         'LayerName': PropertyType(False, one_of(is_str(), is_type(dict))),
         'Description': PropertyType(False, is_str()),
         'ContentUri': PropertyType(True, one_of(is_str(), is_type(dict))),
-#        'CompatibleRuntimes': PropertyType(False, list_of(is_str())),
-        'CompatibleRuntimes': PropertyType(False, list_of(one_of(is_str(), is_type(dict)))),
-##        'CompatibleRuntimes': PropertyType(False, one_of(is_str(), list_of(one_of(is_str(), is_type(dict), is_type(dict))))),
+        'CompatibleRuntimes': PropertyType(False, list_of( one_of( is_str(), is_type(dict) ) )),
         'LicenseInfo': PropertyType(False, is_str()),
         'RetentionPolicy': PropertyType(False, is_str())
     }
