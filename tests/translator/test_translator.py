69db04176db6c539bec4cab9a6093ce08b9bd960
Praneeta <praneep@amazon.com>
2019-05-21 16:11:52 -0700

    Basic test for OpenApiVersion flag to make sure the translator doesnt bail when it's defined
    https://sim.amazon.com/issues/Squirrel-496

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -191,6 +191,7 @@ class TestTranslatorEndToEnd(TestCase):
         'api_with_gateway_responses_minimal',
         'api_with_gateway_responses_implicit',
         'api_with_gateway_responses_string_status_code',
+        'api_with_open_api_version',
         'api_cache',
         'api_with_access_log_setting',
         'api_with_canary_setting',
