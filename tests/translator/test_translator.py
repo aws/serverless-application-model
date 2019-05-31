4fcee9806c762a5ce6447a6c4719a154fd65526e
praneetap <praneeta.prakash@gmail.com>
2019-05-30 21:51:38 -0700

    More tests and cr comments

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -324,6 +324,8 @@ class TestTranslatorEndToEnd(TestCase):
         'api_with_gateway_responses_all_openapi_3',
         'api_with_open_api_version',
         'api_with_open_api_version_2',
+        'api_with_auth_all_minimum_openapi',
+        'api_with_auth_no_openapi_flag'
       ],
       [
        ("aws", "ap-southeast-1"),
