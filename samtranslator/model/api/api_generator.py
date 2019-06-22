82fb271caab27e9e256a837a76988d8f6240b84a
praneetap <praneeta.prakash@gmail.com>
2019-06-21 22:10:02 -0700

    fix: openapi postprocess for request models change

--- samtranslator/model/api/api_generator.py
+++ samtranslator/model/api/api_generator.py
@@ -314,29 +314,7 @@ class ApiGenerator(object):
 
         # Assign the Swagger back to template
 
-        self.definition_body = self._openapi_auth_postprocess(swagger_editor.swagger)
-
-    def _openapi_auth_postprocess(self, definition_body):
-        """
-        Convert auth components to openapi 3 in definition body if OpenApiVersion flag is specified.
-
-        If there is swagger defined in the definition body, we treat it as a swagger spec and do not
-        make any openapi 3 changes to it.
-        """
-        if definition_body.get('swagger') is not None:
-            return definition_body
-
-        if definition_body.get('openapi') is not None:
-            if self.open_api_version is None:
-                self.open_api_version = definition_body.get('openapi')
-
-        if self.open_api_version and re.match(SwaggerEditor.get_openapi_version_3_regex(), self.open_api_version):
-            if definition_body.get('securityDefinitions'):
-                components = definition_body.get('components', {})
-                components['securitySchemes'] = definition_body['securityDefinitions']
-                definition_body['components'] = components
-                del definition_body['securityDefinitions']
-        return definition_body
+        self.definition_body = self._openapi_postprocess(swagger_editor.swagger)
 
     def _add_gateway_responses(self):
         """
@@ -407,9 +385,9 @@ class ApiGenerator(object):
 
         # Assign the Swagger back to template
 
-        self.definition_body = self._openapi_models_postprocess(swagger_editor.swagger)
+        self.definition_body = self._openapi_postprocess(swagger_editor.swagger)
 
-    def _openapi_models_postprocess(self, definition_body):
+    def _openapi_postprocess(self, definition_body):
         """
         Convert definitions to openapi 3 in definition body if OpenApiVersion flag is specified.
 
@@ -424,6 +402,11 @@ class ApiGenerator(object):
                 self.open_api_version = definition_body.get('openapi')
 
         if self.open_api_version and re.match(SwaggerEditor.get_openapi_version_3_regex(), self.open_api_version):
+            if definition_body.get('securityDefinitions'):
+                components = definition_body.get('components', {})
+                components['securitySchemes'] = definition_body['securityDefinitions']
+                definition_body['components'] = components
+                del definition_body['securityDefinitions']
             if definition_body.get('definitions'):
                 components = definition_body.get('components', {})
                 components['schemas'] = definition_body['definitions']
