db1b88722e55f675158ba6cadd303718f4cc4bd9
TDaglis <ath.daglis@gmail.com>
2019-08-06 15:03:36 +0000

    Add testcase for AWS IAM API with no-auth route

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -159,6 +159,7 @@ class TestTranslatorEndToEnd(TestCase):
         'api_with_auth_all_minimum',
         'api_with_auth_no_default',
         'api_with_default_aws_iam_auth',
+        'api_with_default_aws_iam_auth_and_no_auth_route',
         'api_with_method_aws_iam_auth',
         'api_with_aws_iam_auth_overrides',
         'api_with_method_settings',
