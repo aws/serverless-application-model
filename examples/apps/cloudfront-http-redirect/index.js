09b799ef68ee1311e328dce6ebbc08995b681d7b
kennyk <kenny@kremerman.me>
2019-08-27 17:56:14 +0200

    docs: fix malformed response in cloudfront-http-redirect (#1096)

--- examples/apps/cloudfront-http-redirect/index.js
+++ examples/apps/cloudfront-http-redirect/index.js
@@ -1,18 +1,15 @@
 'use strict';
 
 exports.handler = (event, context, callback) => {
-    /*
-     * Generate HTTP redirect response with 302 status code and Location header.
-     */
-    const response = {
-        status: '302',
-        statusDescription: 'Found',
-        headers: {
-            location: [{
-                key: 'Location',
-                value: 'http://docs.aws.amazon.com/lambda/latest/dg/lambda-edge.html',
-            }],
+    
+    var response = {
+        "statusCode": 302,
+        "headers": {
+            "Location": "http://docs.aws.amazon.com/lambda/latest/dg/lambda-edge.html"
         },
+        "body": "{}",
+        "isBase64Encoded": false
     };
     callback(null, response);
 };
+
