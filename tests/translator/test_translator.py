1e4e57f14bf71dc41e2c8fcfa8000f1695725771
Nicolas Moutschen <moutschn@amazon.com>
2020-01-10 21:50:52 +0100

    feat(policy-templates): remove dynamodb:DeleteItem from DynamoDBWritePolicy and s3:DeleteObject from S3WritePolicy

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -311,7 +311,7 @@ class TestTranslatorEndToEnd(TestCase):
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
 
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         # Only update the deployment Logical Id hash in Py3.
         if sys.version_info.major >= 3:
@@ -384,7 +384,7 @@ class TestTranslatorEndToEnd(TestCase):
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
 
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         # Only update the deployment Logical Id hash in Py3.
         if sys.version_info.major >= 3:
@@ -442,7 +442,7 @@ class TestTranslatorEndToEnd(TestCase):
             }
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         # Only update the deployment Logical Id hash in Py3.
         if sys.version_info.major >= 3:
@@ -704,7 +704,7 @@ def test_swagger_body_sha_gets_recomputed():
 
     output_fragment = transform(document, parameter_values, mock_policy_loader)
 
-    print(json.dumps(output_fragment, indent=2))
+    print (json.dumps(output_fragment, indent=2))
     deployment_key = get_deployment_key(output_fragment)
     assert deployment_key
 
@@ -740,7 +740,7 @@ def test_swagger_definitionuri_sha_gets_recomputed():
 
     output_fragment = transform(document, parameter_values, mock_policy_loader)
 
-    print(json.dumps(output_fragment, indent=2))
+    print (json.dumps(output_fragment, indent=2))
     deployment_key = get_deployment_key(output_fragment)
     assert deployment_key
 
@@ -822,7 +822,7 @@ class TestFunctionVersionWithParameterReferences(TestCase):
         mock_policy_loader = get_policy_mock()
         output_fragment = transform(document, parameter_values, mock_policy_loader)
 
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         return output_fragment
 
