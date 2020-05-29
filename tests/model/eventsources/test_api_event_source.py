d168f371f494196a57032313075db9faae5587e4
Sriram Madapusi Vasudevan <3770774+sriram-mv@users.noreply.github.com>
2020-05-29 15:16:46 -0700

    fix: resource policy generation for {path+} (#1580)

--- tests/model/eventsources/test_api_event_source.py
+++ tests/model/eventsources/test_api_event_source.py
@@ -52,6 +52,23 @@ class ApiEventSource(TestCase):
         self.assertEqual(arn, "arn:aws:execute-api:${AWS::Region}:${AWS::AccountId}:${__ApiId__}/${__Stage__}/GET/foo")
 
     @patch("boto3.session.Session.region_name", "eu-west-2")
+    def test_get_permission_with_path_parameter_to_any_path(self):
+        self.api_event_source.Path = "/foo/{userId+}"
+        cfn = self.api_event_source.to_cloudformation(function=self.func, explicit_api={})
+
+        perm = cfn[0]
+        self.assertIsInstance(perm, LambdaPermission)
+
+        try:
+            arn = self._extract_path_from_arn("{}PermissionProd".format(self.logical_id), perm)
+        except AttributeError:
+            self.fail("Permission class isn't valid")
+
+        self.assertEqual(
+            arn, "arn:aws:execute-api:${AWS::Region}:${AWS::AccountId}:${__ApiId__}/${__Stage__}/GET/foo/*"
+        )
+
+    @patch("boto3.session.Session.region_name", "eu-west-2")
     def test_get_permission_with_path_parameter(self):
         self.api_event_source.Path = "/foo/{userId}/bar"
         cfn = self.api_event_source.to_cloudformation(function=self.func, explicit_api={})
