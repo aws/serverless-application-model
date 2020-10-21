ce63ccccb0021d5a6a8377545aade67363657500
Tarun <c2tarun@users.noreply.github.com>
2020-10-21 10:33:30 -0700

    fix: adding support for passing target id to EventBridgeRule

---
+++ tests/model/eventsources/test_cloudwatch_event_source.py
@@ -0,0 +1,24 @@
+from mock import Mock, patch
+from unittest import TestCase
+
+from samtranslator.model.eventsources.push import CloudWatchEvent
+from samtranslator.model.lambda_ import LambdaFunction
+
+
+class CloudWatchEventSourceTests(TestCase):
+    def setUp(self):
+        self.logical_id = "EventLogicalId"
+        self.func = LambdaFunction("func")
+
+    def test_target_id_when_not_provided(self):
+        cloudwatch_event_source = CloudWatchEvent(self.logical_id)
+        cfn = cloudwatch_event_source.to_cloudformation(function=self.func)
+        target_id = cfn[0].Targets[0]["Id"]
+        self.assertEqual(target_id, "{}{}".format(self.logical_id, "LambdaTarget"))
+
+    def test_target_id_when_provided(self):
+        cloudwatch_event_source = CloudWatchEvent(self.logical_id)
+        cloudwatch_event_source.Target = {"Id": "MyTargetId"}
+        cfn = cloudwatch_event_source.to_cloudformation(function=self.func)
+        target_id = cfn[0].Targets[0]["Id"]
+        self.assertEqual(target_id, "MyTargetId")
