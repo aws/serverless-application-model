c1463fe41a88aa723013b40b4855b903198f8394
Kaidi He <73141777+kaidih@users.noreply.github.com>
2020-10-27 13:42:10 -0700

    Mq event source (#60)
    * Support AmazonMQ as event source
    
    * Set black hook language version to python3

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -157,6 +157,7 @@ class TestTranslatorEndToEnd(TestCase):
                 "cloudwatchlog",
                 "streams",
                 "sqs",
+                "amq",
                 "simpletable",
                 "simpletable_with_sse",
                 "implicit_api",
@@ -338,7 +339,7 @@ class TestTranslatorEndToEnd(TestCase):
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
 
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         # Only update the deployment Logical Id hash in Py3.
         if sys.version_info.major >= 3:
@@ -420,7 +421,7 @@ class TestTranslatorEndToEnd(TestCase):
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
 
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         # Only update the deployment Logical Id hash in Py3.
         if sys.version_info.major >= 3:
@@ -479,7 +480,7 @@ class TestTranslatorEndToEnd(TestCase):
             }
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         # Only update the deployment Logical Id hash in Py3.
         if sys.version_info.major >= 3:
@@ -604,6 +605,7 @@ class TestTranslatorEndToEnd(TestCase):
         "error_function_invalid_request_parameters",
         "error_invalid_logical_id",
         "error_layer_invalid_properties",
+        "error_missing_broker",
         "error_missing_queue",
         "error_missing_startingposition",
         "error_missing_stream",
@@ -757,7 +759,7 @@ def test_swagger_body_sha_gets_recomputed():
 
     output_fragment = transform(document, parameter_values, mock_policy_loader)
 
-    print(json.dumps(output_fragment, indent=2))
+    print (json.dumps(output_fragment, indent=2))
     deployment_key = get_deployment_key(output_fragment)
     assert deployment_key
 
@@ -793,7 +795,7 @@ def test_swagger_definitionuri_sha_gets_recomputed():
 
     output_fragment = transform(document, parameter_values, mock_policy_loader)
 
-    print(json.dumps(output_fragment, indent=2))
+    print (json.dumps(output_fragment, indent=2))
     deployment_key = get_deployment_key(output_fragment)
     assert deployment_key
 
@@ -875,7 +877,7 @@ class TestFunctionVersionWithParameterReferences(TestCase):
         mock_policy_loader = get_policy_mock()
         output_fragment = transform(document, parameter_values, mock_policy_loader)
 
-        print(json.dumps(output_fragment, indent=2))
+        print (json.dumps(output_fragment, indent=2))
 
         return output_fragment
 
