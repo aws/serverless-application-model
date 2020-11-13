896ef40063240a446b3b31ceb03e7bcb6c21afa9
Mingkun He <hmingku@amazon.com>
2020-11-13 15:46:18 -0800

    add extra test class and update yaml load call

---
+++ tests_integ/single/test_basic_state_machine.py
@@ -0,0 +1,12 @@
+from parameterized import parameterized
+
+from tests_integ.helpers.base_test import BaseTest
+
+
+class TestBasicLayerVersion(BaseTest):
+    @parameterized.expand([
+        "basic_state_machine_inline_definition",
+        # ("basic_state_machine_with_tags"), # cannot be translated by sam-tran
+    ])
+    def test_basic_state_machine(self, file_name):
+        self.create_and_verify_stack(file_name)
