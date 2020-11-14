b45a890f9bfa2bc1f52f4f5bd385d57a83050f60
Mingkun He <hmingku@amazon.com>
2020-11-13 16:58:06 -0800

    fix bug, now allow create bucket in multi regions

--- tests_integ/helpers/helpers.py
+++ tests_integ/helpers/helpers.py
@@ -4,6 +4,7 @@ import re
 from functools import reduce
 
 import boto3
+from botocore.exceptions import ClientError
 
 from samtranslator.model.exceptions import InvalidDocumentException
 from samtranslator.translator.managed_policy_translator import ManagedPolicyLoader
@@ -53,3 +54,32 @@ def verify_stack_resources(expected_file_path, stack_resources):
 
 def _sort_resources(resources):
     return sorted(resources, key=lambda d: d["LogicalResourceId"])
+
+
+def create_bucket(bucket_name, region=None):
+    """Create an S3 bucket in a specified region
+
+    copy code from boto3 doc example
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created, else False
+    """
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
