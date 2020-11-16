4ae155dd76f9c3c2fa4dc397e34131312d321e1d
Mathieu Grandis <grmathie@amazon.com>
2020-11-16 09:39:18 -0800

    chore: Black formatting

--- tests_integ/single/test_basic_function.py
+++ tests_integ/single/test_basic_function.py
@@ -5,18 +5,20 @@ from tests_integ.helpers.base_test import BaseTest
 
 
 class TestBasicFunction(BaseTest):
-    @parameterized.expand([
-        "basic_function",
-        "basic_function_event_destinations",
-        "basic_function_no_envvar",
-        "basic_function_openapi",
-        "basic_function_with_kmskeyarn",
-        "basic_function_with_sns_dlq",
-        "basic_function_with_sqs_dlq",
-        "basic_function_with_tags",
-        # ("basic_function_with_tracing"), # need different set up to create changeset
-        # ("basic_table_no_param"), # no test case in java code base
-        # ("basic_table_with_param") # no test case in java code base
-    ])
+    @parameterized.expand(
+        [
+            "basic_function",
+            "basic_function_event_destinations",
+            "basic_function_no_envvar",
+            "basic_function_openapi",
+            "basic_function_with_kmskeyarn",
+            "basic_function_with_sns_dlq",
+            "basic_function_with_sqs_dlq",
+            "basic_function_with_tags",
+            # ("basic_function_with_tracing"), # need different set up to create changeset
+            # ("basic_table_no_param"), # no test case in java code base
+            # ("basic_table_with_param") # no test case in java code base
+        ]
+    )
     def test_basic_function(self, file_name):
         self.create_and_verify_stack(file_name)
--- tests_integ/single/test_basic_layer_version.py
+++ tests_integ/single/test_basic_layer_version.py
@@ -4,9 +4,11 @@ from tests_integ.helpers.base_test import BaseTest
 
 
 class TestBasicLayerVersion(BaseTest):
-    @parameterized.expand([
-        "basic_layer",
-        "basic_layer_with_parameters",
-    ])
+    @parameterized.expand(
+        [
+            "basic_layer",
+            "basic_layer_with_parameters",
+        ]
+    )
     def test_basic_layer_version(self, file_name):
         self.create_and_verify_stack(file_name)
