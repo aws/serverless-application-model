6da6a5b96fb1598d36ee89b397204389a7f65263
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2021-01-29 16:08:16 -0800

    Release v1.34.0 (#1893) (#1906)
    * Support DLQ, RetryPolicy properties for EventBridgeRule,Schedule event sources (#1842)
    
    * Add DeadLetterConfig,RetryPolicy properties for EventBridgeRule,Schedule event sources
    
    * Minor fix,rename function argument
    
    * Update test class name
    
    * Combine dlq extraction/generation into the utility class
    
    * Remove unused import
    
    * fix: propagate condition to sqs queue policy for sqssubscription (#1798)
    
    * fix: propagate condition to sqs queue policy for sqssubscription
    
    * Update unit test for function_event_conditions
    
    * Update black commands in Makefile to check only .py files
    
    * Update test with one more SNS event source with sqsSubscription set
    
    * Revert "Update black commands in Makefile to check only .py files"
    
    This reverts commit 115ff09cd1db5939d21ef4d3c8c8e5690629f9ab.
    
    * chore: Remove biased language from pylintrc (#1847)
    
    * fix: Support new CodeDeploy ManagedPolicy (#1858)
    
    * fix: Support new CodeDeploy MangedPolicy in regions without AWSCodeDeployRoleForLambda
    
    CodeDeploy is migrating from AWSCodeDeployRoleForLambda to AWSCodeDeployRoleForLambdaLimited.
    Some partitions do not support AWSCodeDeployRoleForLambda and therefore we need to use the newer
    one in those partitions. We cannot widely update to AWSCodeDeployRoleForLambdaLimited since this
    can cause customer's stacks to fail unexpectedly.
    
    * Forgot to commit unit tests
    
    * Handle PR feedback
    
    Co-authored-by: Jacob Fuss <jfuss@users.noreply.github.com>
    
    * fix: Update Slack invite link (#1877)
    
    Co-authored-by: Jacob Fuss <jfuss@users.noreply.github.com>
    
    * feature: Support for custom checkpointing (#1883)
    
    Co-authored-by: Vinayak <vinayaks@amazon.com>
    
    * Fix: Description in AWS::Serverless::HttpApi (#1884)
    
    * Fix: Description in AWS::Serverless::HttpApi
    
    * Update _set to _add
    
    * Update AWS::S3::Bucket properties (#1885)
    
    * Update AWS::S3::Bucket properties
    
    * Fix type checking validators for AWS::S3::Bucket
    
    * Update to use any_type() in favor of supporing ref
    
    * Fix: Replaced invalid AMQ managed policy by providing policy statements (#1891)
    
    * Fix for invalid MQ event source managed policy
    
    * Fix for invalid managed policy for MQ, included support for new MQ event source property, updated test cases
    
    * Black reformatting
    
    * Test case changes
    
    * Changed policy name
    
    * Modified test cases with new policy name
    
    * chore: bump version 1.34.0 (#1892)
    
    * Fix: SAM crashes method_definition for path is invalid (#1802)
    
    * Fix: SAM crashes method_definition for path is invalid
    
    * Fix: SAM crashes whenmappings is null
    
    * Removed print statement in test_translator
    
    * Fix: Swagger security not a dict
    
    Co-authored-by: Mufaddal Makati <mmmakati@amazon.com>
    
    Co-authored-by: ejafarli <54083696+ejafarli@users.noreply.github.com>
    Co-authored-by: _sam <3804518+aahung@users.noreply.github.com>
    Co-authored-by: Jacob Fuss <32497805+jfuss@users.noreply.github.com>
    Co-authored-by: Jacob Fuss <jfuss@users.noreply.github.com>
    Co-authored-by: vinayaksood <vinayaksood.282@gmail.com>
    Co-authored-by: Vinayak <vinayaks@amazon.com>
    Co-authored-by: Qingchuan Ma <69653965+qingchm@users.noreply.github.com>
    Co-authored-by: Mufaddal Makati <mufaddal@rawbytes.com>
    Co-authored-by: Mufaddal Makati <mmmakati@amazon.com>
    
    Co-authored-by: ejafarli <54083696+ejafarli@users.noreply.github.com>
    Co-authored-by: _sam <3804518+aahung@users.noreply.github.com>
    Co-authored-by: Jacob Fuss <32497805+jfuss@users.noreply.github.com>
    Co-authored-by: Jacob Fuss <jfuss@users.noreply.github.com>
    Co-authored-by: vinayaksood <vinayaksood.282@gmail.com>
    Co-authored-by: Vinayak <vinayaks@amazon.com>
    Co-authored-by: Qingchuan Ma <69653965+qingchm@users.noreply.github.com>
    Co-authored-by: Mufaddal Makati <mufaddal@rawbytes.com>
    Co-authored-by: Mufaddal Makati <mmmakati@amazon.com>

---
+++ tests/unit/test_region_configuration.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+
+from mock import patch
+from parameterized import parameterized
+
+from samtranslator.region_configuration import RegionConfiguration
+
+
+class TestRegionConfiguration(TestCase):
+    @parameterized.expand(
+        [
+            ["aws"],
+        ]
+    )
+    def test_when_apigw_edge_configuration_supported(self, partition):
+
+        with patch(
+            "samtranslator.translator.arn_generator.ArnGenerator.get_partition_name"
+        ) as get_partition_name_patch:
+            get_partition_name_patch.return_value = partition
+
+            self.assertTrue(RegionConfiguration.is_apigw_edge_configuration_supported())
+
+    @parameterized.expand(
+        [
+            ["aws-cn"],
+            ["aws-us-gov"],
+            ["aws-iso"],
+            ["aws-iso-b"],
+        ]
+    )
+    def test_when_apigw_edge_configuration_is_not_supported(self, partition):
+        with patch(
+            "samtranslator.translator.arn_generator.ArnGenerator.get_partition_name"
+        ) as get_partition_name_patch:
+            get_partition_name_patch.return_value = partition
+
+            self.assertFalse(RegionConfiguration.is_apigw_edge_configuration_supported())
