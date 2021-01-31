c61be2722e313d07c077292e168741b448ac51c3
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2021-01-30 23:01:03 -0800

    Update test output with consistent logical ID

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -353,11 +353,6 @@ class TestTranslatorEndToEnd(TestCase):
 
         print(json.dumps(output_fragment, indent=2))
 
-        # Only update the deployment Logical Id hash in Py3.
-        if sys.version_info.major >= 3:
-            self._update_logical_id_hash(expected)
-            self._update_logical_id_hash(output_fragment)
-
         assert deep_sort_lists(output_fragment) == deep_sort_lists(expected)
 
     @parameterized.expand(
@@ -436,11 +431,6 @@ class TestTranslatorEndToEnd(TestCase):
 
         print(json.dumps(output_fragment, indent=2))
 
-        # Only update the deployment Logical Id hash in Py3.
-        if sys.version_info.major >= 3:
-            self._update_logical_id_hash(expected)
-            self._update_logical_id_hash(output_fragment)
-
         assert deep_sort_lists(output_fragment) == deep_sort_lists(expected)
 
     @parameterized.expand(
@@ -495,10 +485,6 @@ class TestTranslatorEndToEnd(TestCase):
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
         print(json.dumps(output_fragment, indent=2))
 
-        # Only update the deployment Logical Id hash in Py3.
-        if sys.version_info.major >= 3:
-            self._update_logical_id_hash(expected)
-            self._update_logical_id_hash(output_fragment)
         assert deep_sort_lists(output_fragment) == deep_sort_lists(expected)
 
     def _update_logical_id_hash(self, resources):
