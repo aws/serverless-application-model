b134e9e33cac3ac8c1284290f332b0379928c9dd
daftster <daftster@yahoo.com>
2021-02-24 17:57:27 -0500

    Update test_translator.py
    remove from test case error_api_invalid_auth as this should be valid.

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -539,7 +539,6 @@ class TestTranslatorEndToEnd(TestCase):
         "error_api_gateway_responses_nonnumeric_status_code",
         "error_api_gateway_responses_unknown_responseparameter",
         "error_api_gateway_responses_unknown_responseparameter_property",
-        "error_api_invalid_auth",
         "error_api_invalid_path",
         "error_api_invalid_definitionuri",
         "error_api_invalid_definitionbody",
