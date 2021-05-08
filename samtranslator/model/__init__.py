2fed3a895d8ea6844d11b206ad80f31346002cf6
Qingchuan Ma <69653965+qingchm@users.noreply.github.com>
2021-05-08 12:55:34 -0700

    feat: Resource level attributes support (#2008)
    * Fix for invalid MQ event source managed policy
    
    * Fix for invalid managed policy for MQ, included support for new MQ event source property, updated test cases
    
    * Black reformatting
    
    * Test case changes
    
    * Changed policy name
    
    * Modified test cases with new policy name
    
    * Added resource attributes and unit tests
    
    * Resource attributes initial work
    
    * Passthrough attributes for some resources, updated some tests
    
    * Resolve merge conflicts
    
    * Fixed a typo
    
    * Modified implicit api plugin for resource attributes support
    
    * Partial update of the tests
    
    * Partially updated test cases, black reformatted
    
    * Partially updated test templates
    
    * Partially updated test templates
    
    * Partially updated test templates
    
    * Added event bridge support for passthrough resource attributes
    
    * Partially updated test templates (up to function with amq kms)
    
    * Partially updated test templates (up to sns)
    
    * Partially updated test templates (all the ones left)
    
    * Prevented passthrough resource attributes from changing layer version hashes
    
    * Added test to verify resource passthrough precedence for implicit api
    
    * Modified tests related to lambda layer to revert the hash changes, keeping the hash the same with resource attributes added
    
    * fix: mutable default values in method definitions (#1997)
    
    * fix: remove explicit logging level set in single module (#1998)
    
    * run automated tests for resource level attribute support
    
    * Skipping metadata in layer hashing
    
    * Refactored the classes for TestTranslatorEndToEnd and TestResourceLevelAttributes to share the same parent class
    
    * Added new translator tests for version and layer resources
    
    * Added new unit tests
    
    * Removed after transform resource plugin
    
    * Black reformatting
    
    * Refactoring implicit api plugin support for DeletionPolicy and UpdateReplacePolicy
    
    * Refactoring to improve code quality
    
    * Added simple documentation
    
    * Black reformatting
    
    * Added input template that was missing
    
    * Refactoring: use sets instead of lists for implicit api plugin
    
    * Changing import to be compatible with py2.7
    
    * Changing test deployment hashes to their actual values
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>

--- samtranslator/model/__init__.py
+++ samtranslator/model/__init__.py
@@ -43,7 +43,11 @@ class Resource(object):
     property_types = None
     _keywords = ["logical_id", "relative_id", "depends_on", "resource_attributes"]
 
-    _supported_resource_attributes = ["DeletionPolicy", "UpdatePolicy", "Condition"]
+    # For attributes in this list, they will be passed into the translated template for the same resource itself.
+    _supported_resource_attributes = ["DeletionPolicy", "UpdatePolicy", "Condition", "UpdateReplacePolicy", "Metadata"]
+    # For attributes in this list, they will be passed into the translated template for the same resource,
+    # as well as all the auto-generated resources that are created from this resource.
+    _pass_through_attributes = ["Condition", "DeletionPolicy", "UpdateReplacePolicy"]
 
     # Runtime attributes that can be qureied resource. They are CloudFormation attributes like ARN, Name etc that
     # will be resolvable at runtime. This map will be implemented by sub-classes to express list of attributes they
@@ -75,6 +79,22 @@ class Resource(object):
         if attributes is not None:
             for attr, value in attributes.items():
                 self.set_resource_attribute(attr, value)
+
+    @classmethod
+    def get_supported_resource_attributes(cls):
+        """
+        A getter method for the supported resource attributes
+        returns: a tuple that contains the name of all supported resource attributes
+        """
+        return tuple(cls._supported_resource_attributes)
+
+    @classmethod
+    def get_pass_through_attributes(cls):
+        """
+        A getter method for the resource attributes to be passed to auto-generated resources
+        returns: a tuple that contains the name of all pass through attributes
+        """
+        return tuple(cls._pass_through_attributes)
 
     @classmethod
     def from_dict(cls, logical_id, resource_dict, relative_id=None, sam_plugins=None):
@@ -318,9 +338,10 @@ class Resource(object):
 
         :return: Dictionary of resource attributes.
         """
-        attributes = None
-        if "Condition" in self.resource_attributes:
-            attributes = {"Condition": self.resource_attributes["Condition"]}
+        attributes = {}
+        for resource_attribute in self.get_pass_through_attributes():
+            if resource_attribute in self.resource_attributes:
+                attributes[resource_attribute] = self.resource_attributes.get(resource_attribute)
         return attributes
 
 
