15d3b710d7f25285617ef227c10b06fa460a1ac4
Qingchuan Ma <qingchm@amazon.com>
2021-05-19 20:26:16 -0700

    Black reformatting

--- tests/unit/model/api/TestSharedApiUsagePlan.py
+++ tests/unit/model/api/TestSharedApiUsagePlan.py
@@ -7,7 +7,6 @@ from samtranslator.model.exceptions import InvalidTemplateException
 
 
 class TestSharedApiUsagePlan(TestCase):
-
     def setUp(self):
         self.shared_usage_plan = SharedApiUsagePlan()
 
@@ -19,7 +18,7 @@ class TestSharedApiUsagePlan(TestCase):
                 "DeletionPolicy": "Delete",
                 "UpdateReplacePolicy": "Delete",
             },
-            conditions
+            conditions,
         )
 
         self.assertEqual(
@@ -28,25 +27,26 @@ class TestSharedApiUsagePlan(TestCase):
                 "DeletionPolicy": "Delete",
                 "UpdateReplacePolicy": "Delete",
             },
-            actual_attributes
+            actual_attributes,
         )
 
-        self.assertEqual(conditions, {'SharedUsagePlanCondition': 'C1'})
+        self.assertEqual(conditions, {"SharedUsagePlanCondition": "C1"})
 
-    @parameterized.expand([
-        ([None], ),
-        ([None, "C1"], ),
-        (["C1", None], ),
-        (["C1", "C2"], ),
-        (["C1", "C2", "C3"], ),
-    ])
+    @parameterized.expand(
+        [
+            ([None],),
+            ([None, "C1"],),
+            (["C1", None],),
+            (["C1", "C2"],),
+            (["C1", "C2", "C3"],),
+        ]
+    )
     def test_multiple_apis_with_conditions(self, api_conditions):
         template_conditions = dict()
         result = {}
         for api_condition in api_conditions:
             result = self.shared_usage_plan.get_combined_resource_attributes(
-                {"Condition": api_condition},
-                template_conditions
+                {"Condition": api_condition}, template_conditions
             )
             print(f"Calling with {api_condition} result {result}")
 
@@ -59,100 +59,61 @@ class TestSharedApiUsagePlan(TestCase):
             self.assertEqual({"Condition": "SharedUsagePlanCondition"}, result)
             combined_conditions = [
                 condition.get("Condition")
-                for condition in
-                template_conditions.get("SharedUsagePlanCondition", {}).get("Fn::Or")
+                for condition in template_conditions.get("SharedUsagePlanCondition", {}).get("Fn::Or")
             ]
             for combined_condition in combined_conditions:
                 self.assertTrue(combined_condition in api_conditions)
 
-
     def test_should_raise_invalid_template_when_no_conditions_section(self):
         with self.assertRaises(InvalidTemplateException):
-            self.shared_usage_plan.get_combined_resource_attributes(
-                {"Condition": "C1"},
-                None
-            )
+            self.shared_usage_plan.get_combined_resource_attributes({"Condition": "C1"}, None)
 
     def test_deletion_policy_priority(self):
         # first api sets it to delete
-        actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "DeletionPolicy": "Delete"
-            },
-            {}
-        )
+        actual_attributes = self.shared_usage_plan.get_combined_resource_attributes({"DeletionPolicy": "Delete"}, {})
         self.assertEqual(actual_attributes["DeletionPolicy"], "Delete")
 
         # then second api sets it to Retain
-        actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "DeletionPolicy": "Retain"
-            },
-            {}
-        )
+        actual_attributes = self.shared_usage_plan.get_combined_resource_attributes({"DeletionPolicy": "Retain"}, {})
         self.assertEqual(actual_attributes["DeletionPolicy"], "Retain")
 
         # if third api sets it to delete, it should keep retain value
-        actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "DeletionPolicy": "Delete"
-            },
-            {}
-        )
+        actual_attributes = self.shared_usage_plan.get_combined_resource_attributes({"DeletionPolicy": "Delete"}, {})
         self.assertEqual(actual_attributes["DeletionPolicy"], "Retain")
 
     def test_update_replace_policy_priority(self):
         # first api sets it to delete
         actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "UpdateReplacePolicy": "Delete"
-            },
-            {}
+            {"UpdateReplacePolicy": "Delete"}, {}
         )
         self.assertEqual(actual_attributes["UpdateReplacePolicy"], "Delete")
 
         # then second api sets it to Retain
         actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "UpdateReplacePolicy": "Snapshot"
-            },
-            {}
+            {"UpdateReplacePolicy": "Snapshot"}, {}
         )
         self.assertEqual(actual_attributes["UpdateReplacePolicy"], "Snapshot")
 
         # if third api sets it to delete, it should keep retain value
         actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "UpdateReplacePolicy": "Delete"
-            },
-            {}
+            {"UpdateReplacePolicy": "Delete"}, {}
         )
         self.assertEqual(actual_attributes["UpdateReplacePolicy"], "Snapshot")
 
         # if third api sets it to delete, it should keep retain value
         actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "UpdateReplacePolicy": "Retain"
-            },
-            {}
+            {"UpdateReplacePolicy": "Retain"}, {}
         )
         self.assertEqual(actual_attributes["UpdateReplacePolicy"], "Retain")
 
         # if third api sets it to delete, it should keep retain value
         actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "UpdateReplacePolicy": "Snapshot"
-            },
-            {}
+            {"UpdateReplacePolicy": "Snapshot"}, {}
         )
         self.assertEqual(actual_attributes["UpdateReplacePolicy"], "Retain")
 
         # if third api sets it to delete, it should keep retain value
         actual_attributes = self.shared_usage_plan.get_combined_resource_attributes(
-            {
-                "UpdateReplacePolicy": "Delete"
-            },
-            {}
+            {"UpdateReplacePolicy": "Delete"}, {}
         )
         self.assertEqual(actual_attributes["UpdateReplacePolicy"], "Retain")
-
