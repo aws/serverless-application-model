6ebf364cd36a800d9b5e1b0f26fff4165200ce44
Manuel Leitold <agrippa1994@users.noreply.github.com>
2021-09-17 03:06:54 +0200

    fix: Allow multiple path segments in BasePath (#2041)

--- tests/model/api/test_http_api_generator.py
+++ tests/model/api/test_http_api_generator.py
@@ -250,15 +250,19 @@ class TestCustomDomains(TestCase):
         self.kwargs["domain"] = {
             "DomainName": "example.com",
             "CertificateArn": "some-url",
-            "BasePath": ["one-1", "two_2", "three", "/"],
+            "BasePath": ["one-1", "two_2", "three", "/", "/api", "api/v1", "api/v1/", "/api/v1/"],
             "Route53": {"HostedZoneId": "xyz", "HostedZoneName": "abc", "IpV6": True},
         }
         http_api = HttpApiGenerator(**self.kwargs)._construct_http_api()
         domain, basepath, route = HttpApiGenerator(**self.kwargs)._construct_api_domain(http_api)
         self.assertIsNotNone(domain, None)
         self.assertIsNotNone(basepath, None)
-        self.assertEqual(len(basepath), 4)
+        self.assertEqual(len(basepath), 8)
         self.assertIsNotNone(route, None)
         self.assertEqual(route.HostedZoneName, None)
         self.assertEqual(route.HostedZoneId, "xyz")
         self.assertEqual(len(route.RecordSets), 2)
+        self.assertEqual(
+            list(map(lambda base: base.ApiMappingKey, basepath)),
+            ["one-1", "two_2", "three", "", "api", "api/v1", "api/v1", "api/v1"],
+        )
