6fdfb47ab0195ac20e371d257f15631420f6438f
Xia Zhao <xazhao@amazon.com>
2021-09-20 13:21:31 -0700

    Change SAR check using boto3

--- samtranslator/region_configuration.py
+++ samtranslator/region_configuration.py
@@ -28,11 +28,9 @@ class RegionConfiguration(object):
     @classmethod
     def is_sar_supported(cls):
         """
-        SAR is not supported in af-south-1 at the moment.
+        SAR is not supported in some regions.
         https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services/
 
         :return: True, if SAR is supported in current region.
         """
-        return boto3.Session().region_name not in [
-            "af-south-1",
-        ]
+        return "serverlessrepo" in boto3.Session().get_available_services()
