c9acc1e8cc8616d4ced06650492805b2700d2117
Mathieu Grandis <73313235+mgrandis@users.noreply.github.com>
2021-10-16 18:59:21 -0700

    feat: Api validation with a new schema (#2032)
    * feat: Validate Resources, AWSTemplateFormatVersion and Transform root elements
    
    * feat: Started implementing Api validation, modified the reported error message to be more meaningful
    
    * feat: Api schema should be complete
    
    * feat: Added test cases
    
    * refactor: Validator error reporting revamp, added more tests, need to fix oneOf Authorizers
    
    * feat: More Api resource validation
    
    * fix: Failing template root tests following the update of the error messages format
    
    * refactor: Simplified the Validator tests, moved them into their own "validator" dir, created input/output files
    
    * feat: Added validation for Cors, Description, Name, TracingEnabled, Variables
    
    * fix: Add the schema_content to the RefResolver to be able to resolve local references
    
    * refactor: Greatly reduced the number of error yaml/json files. Simplified the error format
    
    * feat: Added DefinitionBody
    
    * feat: Added custom type "intrinsic"
    
    * feat: More validation implementation
    
    * feat: More API validation error tests
    
    * feat: More Api implementation, more errors testing, fixing some success tests
    
    * feat: Additional validation, errors order is now consistent across python versions (alpha sorted)
    
    * feat: Update to support intrinsics in additional places, simplify validator
    
    * fix: Black formatting
    
    * fix: Missing path part in the test_translator input folder, Type and Properties are now required on a resource basis, not for all resources anymore, path to Parameter definition
    
    * fix: Allow intrinsics in ReauthorizeEvery, moved required properties for an Api which were defined in the wrong place
    
    * fix: Transform not required anymore, fixed a few test templates
    
    * feat: DependsOn and Condition support on an Api
    
    * fix: Updated translator test outputs following input files fixes
    
    * fix: Translator tests outputs
    
    * fix: Intrinsic functions, api endpoint translator results
    
    * fix: Intrinsic translator test results
    
    * refactor: Restored the static validate method for backward compatibility, added a test that calls both, sorted the Validator Translator input templates test
    
    * feat: Log validation errors as a warning in Parser._validate
    
    * fix: Removed Properties from the template root
    
    * feat: Changed error path separators from / to . (JSON Path style), added tests for Resource level attributes for API, fixed some feedback
    
    * fix: OpenApiVersion validation regex
    
    * refactor: Removed the intrinsics.json schema definition
    
    * feat: Added validation for Resource Type format (x::y::z)
    
    * feat: Added validation for AWS::Serverless:: resources Type
    
    * fix: Added digits to resource Type validation (AWS::S3::Bucket was failing)
    
    * fix: Translator test failing because of new validation on Serverless resources Type
    
    * fix: unsupported_resources.yaml translator test file
    
    * feat: Support for Custom::X resource types
    
    * refactor: Renamed the validate instance method to get_errors to remove ambiguity
    
    * fix: Tags validation

---
+++ tests/validator/test_validator_api.py
@@ -0,0 +1,61 @@
+import os.path
+from parameterized import parameterized
+import pytest
+from unittest import TestCase
+from samtranslator.yaml_helper import yaml_parse
+from samtranslator.validator.validator import SamTemplateValidator
+from tests.validator.test_validator import TestValidatorBase
+
+BASE_PATH = os.path.dirname(__file__)
+INPUT_FOLDER = os.path.join(BASE_PATH, "input", "api")
+OUTPUT_FOLDER = os.path.join(BASE_PATH, "output", "api")
+
+
+class TestValidatorApi(TestValidatorBase):
+    @parameterized.expand(
+        [
+            "error_accesslogsetting",
+            "error_auth",
+            "error_auth_cognito",
+            "error_auth_lambda",
+            "error_auth_lambdarequest_identity",
+            "error_auth_lambdatoken_identity",
+            "error_auth_resourcepolicy",
+            "error_auth_usageplan",
+            "error_binarymediatypes",
+            "error_cachecluster",
+            "error_canarysetting",
+            "error_cors",
+            "error_definitionbody",
+            "error_definitionuri",
+            "error_description",
+            "error_domain",
+            "error_endpointconfiguration",
+            "error_gatewayresponses",
+            "error_methodsettings",
+            "error_minimumcompressionsize",
+            "error_models",
+            "error_name",
+            "error_openapiversion",
+            "error_properties",
+            "error_resource_attributes",
+            "error_stagename",
+            "error_tags",
+            "error_tracingenabled",
+            "error_variables",
+        ],
+    )
+    def test_errors(self, template):
+        self._test_validator_error(os.path.join(INPUT_FOLDER, template), os.path.join(OUTPUT_FOLDER, template))
+
+    @parameterized.expand(
+        [
+            "success_auth_cognito",
+            "success_auth_lambdarequest",
+            "success_complete_api",
+            "success_minimal_api",
+            "success_resource_attributes",
+        ],
+    )
+    def test_success(self, template):
+        self._test_validator_success(os.path.join(INPUT_FOLDER, template))
