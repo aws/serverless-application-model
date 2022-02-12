11a5b25befcfe54b62ec4469d3e3e2a6aca9738b
Marek Buchler <mbuchler@amazon.com>
2022-02-11 16:54:15 -0800

    Add method for determining service availability in a region

--- tests/unit/test_region_configuration.py
+++ tests/unit/test_region_configuration.py
@@ -36,3 +36,27 @@ class TestRegionConfiguration(TestCase):
             get_partition_name_patch.return_value = partition
 
             self.assertFalse(RegionConfiguration.is_apigw_edge_configuration_supported())
+
+    @parameterized.expand(
+        [
+            # use ec2 as it's just about everywhere
+            ["ec2", "cn-north-1"],
+            ["ec2", "us-west-2"],
+            ["ec2", "us-gov-east-1"],
+            ["ec2", "us-isob-east-1"],
+            ["ec2", None],
+            # test SAR since SAM uses that
+            ["serverlessrepo", "us-east-1"],
+            ["serverlessrepo", "ap-southeast-2"],
+        ]
+    )
+    def test_is_service_supported_positive(self, service, region):
+        self.assertTrue(RegionConfiguration.is_service_supported(service, region))
+
+    def test_is_service_supported_negative(self):
+        # use an unknown service name
+        self.assertFalse(RegionConfiguration.is_service_supported("ec1", "us-east-1"))
+        # use a region that does not exist
+        self.assertFalse(RegionConfiguration.is_service_supported("ec2", "us-east-0"))
+        # hard to test with a real service, since the test may start failing once that
+        # service is rolled out to more regions...
