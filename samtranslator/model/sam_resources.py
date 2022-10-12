a39c96ed52ecd16f7a56e3013c5f556eb880da26
Chris Rehn <rehnc@amazon.com>
2022-10-12 13:03:38 -0700

    Replace DependsOn to connector by its generated logical IDs

--- samtranslator/model/sam_resources.py
+++ samtranslator/model/sam_resources.py
@@ -6,6 +6,7 @@ from samtranslator.model.connector.connector import (
     ConnectorResourceReference,
     ConnectorResourceError,
     add_depends_on,
+    replace_depends_on,
     get_event_source_mappings,
     get_resource_reference,
 )
@@ -1710,6 +1711,9 @@ class SamConnector(SamResourceMacro):
             generated_resources.extend(
                 self._construct_lambda_permission_policy(source, destination, profile_properties)
             )
+
+        generated_logical_ids = [resource.logical_id for resource in generated_resources]
+        replace_depends_on(self.logical_id, generated_logical_ids, resource_resolver)
 
         self._add_connector_metadata(generated_resources, original_template, source, destination)
         if generated_resources:
