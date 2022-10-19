7b3a3c139fc06f618e5605d125f96a5c7971fa6b
GZ <hz351086153@gmail.com>
2022-10-18 20:21:14 -0700

    Do not change input file and exit if hardcoded partition used (#2554)
    Co-authored-by: Gavin Zhang <yuanhaoz@amazon.com>

--- bin/add_transform_test.py
+++ bin/add_transform_test.py
@@ -11,63 +11,26 @@ Options:
 """
 import json
 import subprocess
-import re
 import os
 import shutil
 import sys
-import yaml
 import tempfile
 from docopt import docopt  # type: ignore
 from pathlib import Path
 from typing import Any, Dict
-
-from samtranslator.yaml_helper import yaml_parse
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 TRANSFORM_TEST_DIR = os.path.join(SCRIPT_DIR, "..", "tests", "translator")
 CLI_OPTIONS = docopt(__doc__)
 
 
-def map_nested(obj: Any, fn) -> Any:
-    if isinstance(obj, dict):
-        return {k: map_nested(v, fn) for k, v in obj.items()}
-    if isinstance(obj, list):
-        return [map_nested(v, fn) for v in obj]
-    return fn(obj)
-
-
-def replace_arn(s: Any) -> Any:
-    if not isinstance(s, str):
-        return s
-
-    pattern = "arn:aws:"
-    replaced_pattern = "arn:${AWS::Partition}"
-    if pattern in s:
-        # pattern is substring of s, use Fn::Sub to replace part of s
-        s = s.replace(pattern, replaced_pattern)
-    if re.search(r"\${.+}", s):
-        return {"Fn::Sub": s}
-    return s
-
-
-def replace_arn_partitions(input_file_path: str) -> None:
-    with open(input_file_path, "r") as f:
-        sam_template = yaml_parse(f)
-
-    replaced_template = map_nested(sam_template, lambda v: replace_arn(v))
-
-    with open(input_file_path, "w") as f:
-        yaml.dump(replaced_template, f, default_flow_style=False)
-
-
 def read_json_file(file_path: str) -> Dict[str, Any]:
-    with open(file_path, "r") as f:
-        sam_template: Dict[str, Any] = json.load(f)
-    return sam_template
+    template: Dict[str, Any] = json.loads(Path(file_path).read_text(encoding="utf-8"))
+    return template
 
 
 def write_json_file(obj: Dict[str, Any], file_path: str) -> None:
-    with open(file_path, "w") as f:
+    with open(file_path, "w", encoding="utf-8") as f:
         json.dump(obj, f, indent=2)
 
 
@@ -129,14 +92,20 @@ def get_input_file_path() -> str:
 
 def copy_input_file_to_transform_test_dir(input_file_path: str, transform_test_input_path: str) -> None:
     shutil.copyfile(input_file_path, transform_test_input_path)
-
-    replace_arn_partitions(transform_test_input_path)
     print(f"Transform Test input file generated {transform_test_input_path}")
+
+
+def verify_input_template(input_file_path: str):
+    if "arn:aws:" in Path(input_file_path).read_text(encoding="utf-8"):
+        print("ERROR: hardcoded partition name detected. Consider replace it with pseudo parameter {AWS::Partition}")
+        sys.exit(1)
 
 
 def main() -> None:
     input_file_path = get_input_file_path()
     file_basename = Path(input_file_path).stem
+
+    verify_input_template(input_file_path)
 
     transform_test_input_path = os.path.join(TRANSFORM_TEST_DIR, "input", file_basename + ".yaml")
     copy_input_file_to_transform_test_dir(input_file_path, transform_test_input_path)
