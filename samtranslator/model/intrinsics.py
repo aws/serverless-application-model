1ff754166e3184baba84d46555b83d2095087ade
_sam <3804518+aahung@users.noreply.github.com>
2022-10-28 14:01:31 -0700

    Update samtranslator/model/intrinsics.py
    good catch
    
    Co-authored-by: Chris Rehn <1280602+hoffa@users.noreply.github.com>

--- samtranslator/model/intrinsics.py
+++ samtranslator/model/intrinsics.py
@@ -217,12 +217,12 @@ def get_logical_id_from_intrinsic(input: Any) -> Optional[str]:
     if isinstance(v, str):
         return v
 
-    # Fn::GetAtt [<logical-id>, <attribute>]
+    # Fn::GetAtt: [<logical-id>, <attribute>]
     v = input.get("Fn::GetAtt")
     if isinstance(v, list) and len(v) == 2 and isinstance(v[0], str):
         return v[0]
 
-    # Fn::GetAtt <logical-id>.<attribute>
+    # Fn::GetAtt: <logical-id>.<attribute>
     if isinstance(v, str):
         tokens = v.split(".")
         if len(tokens) == 2:
