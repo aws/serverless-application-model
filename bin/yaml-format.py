fee1ab7ae417b707b30422ab88a0871c6a573d0f
Sam Liu <xinhol@amazon.com>
2022-11-16 11:18:47 -0800

    chore: Add --add-test-metadata option to yaml formatter

--- bin/yaml-format.py
+++ bin/yaml-format.py
@@ -2,13 +2,14 @@
 """JSON file formatter (without prettier)."""
 import os
 import sys
+from textwrap import dedent
 
 my_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, my_path + "/..")
 
 import re
 from io import StringIO
-from typing import Type
+from typing import Any, Dict, Type
 
 # We use ruamel.yaml for parsing yaml files because it can preserve comments
 from ruamel.yaml import YAML
@@ -27,10 +28,11 @@ class YAMLFormatter(FileFormatter):
     def description() -> str:
         return "YAML file formatter"
 
-    @staticmethod
-    def format(input_str: str) -> str:
+    def format(self, input_str: str) -> str:
         """Opinionated format YAML file."""
         obj = yaml.load(input_str)
+        if self.args.add_test_metadata:
+            self._add_test_metadata(obj)
         out_stream = StringIO()
         yaml.dump(
             obj,
@@ -40,12 +42,34 @@ class YAMLFormatter(FileFormatter):
         return re.sub(r"\n+$", "\n", out_stream.getvalue())
 
     @staticmethod
+    def _add_test_metadata(obj: Dict[str, Any]) -> None:
+        metadata = obj.get("Metadata", {})
+        if not metadata:
+            metadata = obj["Metadata"] = {}
+        sam_transform_test_value = metadata.get("SamTransformTest")
+        if sam_transform_test_value is not None and sam_transform_test_value is not True:
+            raise ValueError(f"Unexpected Metadata.SamTransformTest value {sam_transform_test_value}")
+        metadata["SamTransformTest"] = True
+
+    @staticmethod
     def decode_exception() -> Type[Exception]:
         return YAMLError
 
     @staticmethod
     def file_extension() -> str:
         return ".yaml"
+
+    @classmethod
+    def config_additional_args(cls) -> None:
+        cls.arg_parser.add_argument(
+            "--add-test-metadata",
+            action="store_true",
+            help=dedent(
+                """\
+                Add the testing metadata to yaml file if it doesn't exist:
+                "Metadata: SamTransformTest: true" """
+            ),
+        )
 
 
 if __name__ == "__main__":
