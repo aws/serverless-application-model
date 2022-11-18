0c5052affb913601586f603cdc40e8231f4d96bb
Chris Rehn <rehnc@amazon.com>
2022-11-17 23:49:27 -0800

    Remove make schema

--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-make schema
+# TODO: Also check transform tests
 find integration/resources -name '*.yaml' | while read -r template; do
     jsonschema -i <(cfn-flip --json "${template}") samtranslator/schema/schema.json
 done
