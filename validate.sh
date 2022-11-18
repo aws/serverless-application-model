f9e888ae55f7c73a69001b47cad107bc36b3cc21
Chris Rehn <rehnc@amazon.com>
2022-11-17 18:29:09 -0800

    Clean slate

--- validate.sh
+++ validate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-find integration/resources -name '*.yaml' | while read -r template; do
+make schema
+#find integration/resources -name '*.yaml' | grep -v connector | while read -r template; do
+echo /Users/rehnc/Desktop/serverless-application-model/integration/resources/templates/combination/connector_bucket_to_function_write.yaml | while read -r template; do
     jsonschema -i <(cfn-flip --json "${template}") samtranslator/schema/schema.json
 done
