460e08643e576ca5eb32eb2d109abdd97b3cd076
Chris Rehn <1280602+hoffa@users.noreply.github.com>
2022-11-29 09:13:18 -0800

    Update any_cfn_resource.py

--- samtranslator/schema/any_cfn_resource.py
+++ samtranslator/schema/any_cfn_resource.py
@@ -4,6 +4,6 @@ from samtranslator.schema.common import LenientBaseModel
 
 constr = pydantic.constr
 
-# Has string Type but is not AWS::Serverless::*
+# Anything goes if has string Type but is not AWS::Serverless::*
 class Resource(LenientBaseModel):
     Type: constr(regex=r"^(?!AWS::Serverless::).+$")  # type: ignore
