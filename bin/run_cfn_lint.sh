4a10f042c4e9bdcebdbe2d95656baad914344f08
Chris Rehn <rehnc@amazon.com>
2022-12-07 14:58:30 -0800

    Add comment

--- bin/run_cfn_lint.sh
+++ bin/run_cfn_lint.sh
@@ -3,6 +3,8 @@ set -eux
 
 VENV=.venv_cfn_lint
 
+# Install to separate venv to avoid circular dependency; cfn-lint depends on samtranslator
+# See https://github.com/aws/serverless-application-model/issues/1042
 if [ ! -d "${VENV}" ]; then
     python3 -m venv "${VENV}"
     "${VENV}/bin/python" -m pip install cfn-lint==0.72.2
