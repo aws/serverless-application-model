579e2ec1d67400c641bd5353c7629a254078c521
Aayush Thapa <aaythapa@amazon.com>
2023-01-26 13:14:43 -0800

    schema update

--- schema_source/aws_serverless_simpletable.py
+++ schema_source/aws_serverless_simpletable.py
@@ -35,3 +35,8 @@ class Resource(ResourceAttributes):
     Type: Literal["AWS::Serverless::SimpleTable"]
     Properties: Optional[Properties]
     Connectors: Optional[Dict[str, EmbeddedConnector]]
+    Condition: Optional[PassThroughProp]
+    DeletionPolicy: Optional[PassThroughProp]
+    UpdateReplacePolicy: Optional[PassThroughProp]
+    DependsOn: Optional[PassThroughProp]
+    Metadata: Optional[PassThroughProp]
