40eb895aff9479732c528dae3e9696945c5e7911
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2023-01-31 14:54:54 -0800

    Refactor

--- bin/list_managed_policies.py
+++ bin/list_managed_policies.py
@@ -1,13 +1,13 @@
 import json
+import re
 
 import boto3
 
-from samtranslator.public.translator import ManagedPolicyLoader
+from samtranslator.translator.managed_policy_translator import ManagedPolicyLoader
 
 
-# TODO: generic replace?
-def foo(arn: str, partition: str) -> str:
-    return arn.replace("arn:aws:", "arn:" + partition + ":")
+def replace_partition(arn: str, partition: str) -> str:
+    return re.sub(r"^arn:\w+:", f"arn:{partition}:", arn)
 
 
 def main() -> None:
@@ -15,8 +15,8 @@ def main() -> None:
     iam = boto3.client("iam", region_name="us-west-2")
 
     policies_aws = ManagedPolicyLoader(iam).load()  # type: ignore
-    policies_aws_cn = {k: foo(v, "aws-cn") for k, v in policies_aws.items()}
-    policies_aws_us_gov = {k: foo(v, "aws-us-gov") for k, v in policies_aws.items()}
+    policies_aws_cn = {k: replace_partition(v, "aws-cn") for k, v in policies_aws.items()}
+    policies_aws_us_gov = {k: replace_partition(v, "aws-us-gov") for k, v in policies_aws.items()}
 
     print(
         json.dumps(
