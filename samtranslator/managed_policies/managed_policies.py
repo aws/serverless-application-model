d500e36636a72bbbb01dd3328ebad744e81bcebb
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2023-01-31 18:29:37 -0800

    Fix tests

--- samtranslator/managed_policies/managed_policies.py
+++ samtranslator/managed_policies/managed_policies.py
@@ -1,15 +1,15 @@
 import json
 import os
 from typing import Dict
-from functools import lru_cache
 
 from samtranslator.translator.arn_generator import ArnGenerator
 
 
-@lru_cache(maxsize=1)
+# TODO: Adding cache breaks tests as partition is reused
+# @lru_cache(maxsize=1)
 def get_managed_policy_map() -> Dict[str, str]:
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "managed_policies.json")
     with open(path, encoding="utf-8") as f:
         policies: Dict[str, str] = json.load(f)
         partition = ArnGenerator.get_partition_name()
-        return {k: v.replace("<partition>", partition) for k, v in policies.items()}
+        return {k: v.format(partition=partition) for k, v in policies.items()}
