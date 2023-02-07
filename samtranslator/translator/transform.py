8c47a55ba929daad1a73ec4af33c63a94ddf9386
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2023-02-07 12:52:23 -0800

    Add back test

--- samtranslator/translator/transform.py
+++ samtranslator/translator/transform.py
@@ -14,7 +14,7 @@ def transform(input_fragment, parameter_values, managed_policy_loader, feature_t
 
     sam_parser = Parser()
     to_py27_compatible_template(input_fragment, parameter_values)  # type: ignore[no-untyped-call]
-    translator = Translator(None, sam_parser)  # type: ignore[no-untyped-call]
+    translator = Translator(managed_policy_loader.load(), sam_parser)  # type: ignore[no-untyped-call]
     transformed = translator.translate(
         input_fragment,
         parameter_values=parameter_values,
