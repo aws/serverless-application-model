38d0446e2763d210c3bc72b190f4a42327bc75c7
Chris Rehn <1280602+hoffa@users.noreply.github.com>
2023-02-08 15:24:22 -0800

    lint-fix

--- samtranslator/internal/managed_policies/managed_policies.py
+++ samtranslator/internal/managed_policies/managed_policies.py
@@ -1,7 +1,7 @@
 import json
 import os
-from typing import Dict
 from pathlib import Path
+from typing import Dict
 
 with Path(os.path.dirname(os.path.abspath(__file__)), "managed_policies.json").open(encoding="utf-8") as f:
     _BUNDLED_MANAGED_POLICIES: Dict[str, Dict[str, str]] = json.load(f)
