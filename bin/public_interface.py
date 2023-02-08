e35a06e188b29dec427464d436277f592d2446eb
Sam Liu <xinhol@amazon.com>
2023-02-07 17:30:14 -0800

    Rename constants to variables

--- bin/public_interface.py
+++ bin/public_interface.py
@@ -13,7 +13,7 @@ from typing import Any, Dict, Set, Union
 class InterfaceScanner:
     def __init__(self) -> None:
         self.signatures: Dict[str, Union[inspect.Signature]] = {}
-        self.constants: Set[str] = set()
+        self.variables: Set[str] = set()
 
     def scan_interfaces_recursively(self, module_name: str) -> None:
         self._scan_interfaces_in_module(module_name)
@@ -24,7 +24,7 @@ class InterfaceScanner:
     def _scan_interfaces_in_module(self, module_name: str) -> None:
         self._scan_functions_in_module(module_name)
         self._scan_classes_in_module(module_name)
-        self._scan_constants_in_module(module_name)
+        self._scan_variables_in_module(module_name)
 
     def _scan_functions_in_module(self, module_name: str) -> None:
         for function_name, function in inspect.getmembers(
@@ -36,7 +36,7 @@ class InterfaceScanner:
             full_path = f"{module_name}.{function_name}"
             self.signatures[full_path] = inspect.signature(function)
 
-    def _scan_constants_in_module(self, module_name: str) -> None:
+    def _scan_variables_in_module(self, module_name: str) -> None:
         """
         There is no method to verify if a module attribute is a constant,
         After some experiment, here we assume if an attribute is a value
@@ -49,7 +49,7 @@ class InterfaceScanner:
             if constant_name.startswith("_"):
                 continue
             full_path = f"{module_name}.{constant_name}"
-            self.constants.add(full_path)
+            self.variables.add(full_path)
 
     def _scan_classes_in_module(self, module_name: str) -> None:
         for class_name, _class in inspect.getmembers(importlib.import_module(module_name), inspect.isclass):
@@ -68,9 +68,9 @@ class InterfaceScanner:
             self.signatures[full_path] = inspect.signature(method)
 
 
-def _save(signature: Dict[str, inspect.Signature], constants: Set[str], output_file: Path) -> None:
+def _save(signature: Dict[str, inspect.Signature], variables: Set[str], output_file: Path) -> None:
     with open(output_file, "w") as f:
-        result: Dict[str, Any] = {"routines": {}, "constants": sorted(constants)}
+        result: Dict[str, Any] = {"routines": {}, "variables": sorted(variables)}
         for key, value in signature.items():
             result["routines"][key] = [
                 {
@@ -99,7 +99,7 @@ def main() -> None:
     if args.command == "extract":
         scanner = InterfaceScanner()
         scanner.scan_interfaces_recursively(args.module)
-        _save(scanner.signatures, scanner.constants, args.output_file)
+        _save(scanner.signatures, scanner.variables, args.output_file)
     # TODO: handle compare
     else:
         parser.print_help()
