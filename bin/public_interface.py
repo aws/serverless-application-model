633ce6ad03c4aeb017d4085f1f812838b9bfc1c7
_sam <3804518+aahung@users.noreply.github.com>
2023-02-09 10:56:48 -0800

    ci: Add GH Action to detect compatibility breaking chages (#2877)

--- bin/public_interface.py
+++ bin/public_interface.py
@@ -90,17 +90,16 @@ class InterfaceScanner:
 def _print(signature: Dict[str, inspect.Signature], variables: Set[str]) -> None:
     result: Dict[str, Any] = {"routines": {}, "variables": sorted(variables)}
     for key, value in signature.items():
-        for parameter in value.parameters.values():
-            result["routines"][key] = [
-                {
-                    "name": parameter.name,
-                    "kind": parameter.kind.name,
-                    "default": parameter.default,
-                }
-                if parameter.default != inspect.Parameter.empty
-                else {"name": parameter.name, "kind": parameter.kind.name}
-                for parameter in value.parameters.values()
-            ]
+        result["routines"][key] = [
+            {
+                "name": parameter.name,
+                "kind": parameter.kind.name,
+                "default": parameter.default,
+            }
+            if parameter.default != inspect.Parameter.empty
+            else {"name": parameter.name, "kind": parameter.kind.name}
+            for parameter in value.parameters.values()
+        ]
     print(json.dumps(result, indent=2, sort_keys=True))
 
 
@@ -132,12 +131,12 @@ class _BreakingChanges(NamedTuple):
             print("\n## Deleted module level variables")
             for name in self.deleted_variables:
                 print(f"- {name}")
-        if self.deleted_variables:
+        if self.deleted_routines:
             print("\n## Deleted routines")
             for name in self.deleted_routines:
                 print(f"- {name}")
         if self.incompatible_routines:
-            print("\n## Deleted routines")
+            print("\n## Incompatible routines")
             for name in self.incompatible_routines:
                 before = f"({', '.join(self._argument_to_str(arg) for arg in original_routines[name])})"
                 after = f"({', '.join(self._argument_to_str(arg) for arg in routines[name])})"
