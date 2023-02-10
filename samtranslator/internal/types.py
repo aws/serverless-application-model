f35c040739e3feddfd2b79cdca729bd9a7523660
Chris Rehn <1280602+hoffa@users.noreply.github.com>
2023-02-10 14:45:52 -0800

    Make function optional

--- samtranslator/internal/types.py
+++ samtranslator/internal/types.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict
+from typing import Callable, Dict, Optional
 
 # Function to retrieve name-to-ARN managed policy map
-GetManagedPolicyMap = Callable[[], Dict[str, str]]
+GetManagedPolicyMap = Callable[[], Optional[Dict[str, str]]]
