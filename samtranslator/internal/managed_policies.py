8429ee80865adb9655c1703a603b255e4f23ce90
Chris Rehn <1280602+hoffa@users.noreply.github.com>
2023-02-13 16:52:10 -0800

    Rename to aws_managed_policies.json

--- samtranslator/internal/managed_policies.py
+++ samtranslator/internal/managed_policies.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 from typing import Dict, Optional
 
-with Path(os.path.dirname(os.path.abspath(__file__)), "data", "managed_policies.json").open(encoding="utf-8") as f:
+with Path(os.path.dirname(os.path.abspath(__file__)), "data", "aws_managed_policies.json").open(encoding="utf-8") as f:
     _BUNDLED_MANAGED_POLICIES: Dict[str, Dict[str, str]] = json.load(f)
 
 
