7647c28c69390fd61eeaee579ae73f19e5e4e760
Pradhapan Rajendran <rajendranpradhapan@gmail.com>
2023-02-13 19:42:44 -0500

    fixed lint issue

--- samtranslator/schema/aws_serverless_graphqlapi.py
+++ samtranslator/schema/aws_serverless_graphqlapi.py
@@ -6,6 +6,7 @@ from samtranslator.schema.common import BaseModel, get_prop, DictStrAny
 
 properties = get_prop("sam-resource-graphqlapi")
 
+
 # TODO: add docs
 class Auth(BaseModel):
     Type: str
