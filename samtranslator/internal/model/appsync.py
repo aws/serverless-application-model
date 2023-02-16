616dd82c3a1df205c8543f571f5f438036a84837
Pradhapan Rajendran <67450797+pradhapanr@users.noreply.github.com>
2023-02-16 14:37:03 -0500

    feat: make AppSync::DataSource mappings (DynamoDB) (#2906)
    Disregarding failed backward compatibility check as the deleted functionality wasn't ever released

---
+++ samtranslator/internal/model/appsync.py
@@ -0,0 +1,74 @@
+from typing import Any, Dict, List, Optional
+
+from typing_extensions import TypedDict
+
+from samtranslator.model import GeneratedProperty, Resource
+from samtranslator.model.intrinsics import fnGetAtt
+from samtranslator.utils.types import Intrinsicable
+
+# Data source constants can be found here under "Type" property:
+# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-appsync-datasource.html
+SUPPORTED_DATASOURCES = {"AMAZON_DYNAMODB"}
+
+
+class DeltaSyncConfigType(TypedDict):
+    BaseTableTTL: str
+    DeltaSyncTableName: str
+    DeltaSyncTableTTL: str
+
+
+class DynamoDBConfigType(TypedDict, total=False):
+    AwsRegion: str
+    TableName: str
+    UseCallerCredentials: bool
+    Versioned: bool
+    DeltaSyncConfig: DeltaSyncConfigType
+
+
+class GraphQLApi(Resource):
+    resource_type = "AWS::AppSync::GraphQLApi"
+    property_types = {
+        "Name": GeneratedProperty(),
+        "Tags": GeneratedProperty(),
+        "XrayEnabled": GeneratedProperty(),
+        "AuthenticationType": GeneratedProperty(),
+    }
+
+    Name: str
+    AuthenticationType: str
+    Tags: Optional[List[Dict[str, Any]]]
+    XrayEnabled: Optional[bool]
+
+    runtime_attrs = {"api_id": lambda self: fnGetAtt(self.logical_id, "ApiId")}
+
+
+class GraphQLSchema(Resource):
+    resource_type = "AWS::AppSync::GraphQLSchema"
+    property_types = {
+        "ApiId": GeneratedProperty(),
+        "Definition": GeneratedProperty(),
+        "DefinitionS3Location": GeneratedProperty(),
+    }
+
+    ApiId: Intrinsicable[str]
+    Definition: Optional[str]
+    DefinitionS3Location: Optional[str]
+
+
+class DataSource(Resource):
+    resource_type = "AWS::AppSync::DataSource"
+    property_types = {
+        "ApiId": GeneratedProperty(),
+        "Description": GeneratedProperty(),
+        "Name": GeneratedProperty(),
+        "Type": GeneratedProperty(),
+        "ServiceRoleArn": GeneratedProperty(),
+        "DynamoDBConfig": GeneratedProperty(),
+    }
+
+    ApiId: Intrinsicable[str]
+    Description: Optional[str]
+    Name: str
+    Type: str
+    ServiceRoleArn: str
+    DynamoDBConfig: DynamoDBConfigType
