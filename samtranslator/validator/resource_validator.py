8f6d3822e3ba64b969af750dd8466e761d74076e
Gavin Zhang <yuanhaoz@amazon.com>
2023-02-17 13:32:26 -0800

    Address Feedback

--- samtranslator/validator/resource_validator.py
+++ samtranslator/validator/resource_validator.py
@@ -9,10 +9,10 @@ T = TypeVar("T", bound=BaseModel)
 # Note: For compabitliy issue, we should ONLY use this with new abstraction/resources.
 def to_model(resource_properties: Dict[Any, Any], cls: Type[T]) -> T:
     """
-    Given properties of a SAM resource return a typed object from the definitions of SAM schema model
+    Given a resource properties, return a typed object from the definitions of SAM schema model
 
     param:
         resource_properties: properties from input template
-        cls: SAM schema models
+        cls: schema models
     """
     return cls.parse_obj(resource_properties)
