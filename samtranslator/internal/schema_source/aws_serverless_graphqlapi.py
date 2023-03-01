d78e92f9852f9d01f080d1f05b37771b9e1f98f9
Pradhapan Rajendran <67450797+pradhapanr@users.noreply.github.com>
2023-03-01 14:05:23 -0500

    feat: make Logging property for GraphQLApi (#2971)

--- samtranslator/internal/schema_source/aws_serverless_graphqlapi.py
+++ samtranslator/internal/schema_source/aws_serverless_graphqlapi.py
@@ -1,8 +1,8 @@
-from typing import Optional
+from typing import Optional, Union
 
 from typing_extensions import Literal
 
-from samtranslator.internal.schema_source.common import BaseModel, DictStrAny, get_prop
+from samtranslator.internal.schema_source.common import BaseModel, DictStrAny, PassThroughProp, get_prop
 
 properties = get_prop("sam-resource-graphqlapi")
 
@@ -12,6 +12,12 @@ class Auth(BaseModel):
     Type: str
 
 
+class Logging(BaseModel):
+    CloudWatchLogsRoleArn: Optional[str]
+    ExcludeVerboseContent: Optional[PassThroughProp]
+    FieldLogLevel: Optional[str]
+
+
 class Properties(BaseModel):
     Auth: Auth
     Tags: Optional[DictStrAny]
@@ -19,6 +25,7 @@ class Properties(BaseModel):
     XrayEnabled: Optional[bool]
     SchemaInline: Optional[str]
     SchemaUri: Optional[str]
+    Logging: Optional[Union[Logging, bool]]
 
 
 class Resource(BaseModel):
