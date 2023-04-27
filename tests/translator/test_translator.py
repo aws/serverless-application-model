3262d71e984e9e2bdbe04238b3761d518a819f81
Chris Rehn <1280602+hoffa@users.noreply.github.com>
2023-04-27 14:21:28 -0700

    Add extra MP

--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -186,6 +186,9 @@ class AbstractTestTranslator(TestCase):
                 mock_policy_loader.load.return_value[
                     "AWSXrayWriteOnlyAccess"
                 ] = "arn:aws-us-gov:iam::aws:policy/AWSXrayWriteOnlyAccess-mock-from-fallback-policy-loader"
+                mock_policy_loader.load.return_value[
+                    "AmazonAPIGatewayPushToCloudWatchLogs"
+                ] = "arn:aws-us-gov:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs-mock-from-fallback-policy-loader"
 
             output_fragment = transform(manifest, parameter_values, mock_policy_loader)
 
