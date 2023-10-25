e528bf2b58514cbf4b9894ea931305dc56fa9d70
Connor Robertson <connorzl@amazon.com>
2023-10-24 17:28:38 -0700

    Fix: Raise Error When UpdateReplacePolicy is not correctly defined. (#3392)

--- samtranslator/plugins/api/implicit_api_plugin.py
+++ samtranslator/plugins/api/implicit_api_plugin.py
@@ -496,8 +496,8 @@ class ImplicitApiPlugin(BasePlugin, Generic[T], metaclass=ABCMeta):
         method_conditions = api_dict_condition.setdefault(path, {})
         method_conditions[method] = condition
 
-        api_dict_deletion = self.api_deletion_policies.setdefault(api_id, set())
-        api_dict_deletion.add(deletion_policy)
+        api_dict_deletion = self.api_deletion_policies.setdefault(api_id, [])
+        api_dict_deletion.append(deletion_policy)
 
-        api_dict_update_replace = self.api_update_replace_policies.setdefault(api_id, set())
-        api_dict_update_replace.add(update_replace_policy)
+        api_dict_update_replace = self.api_update_replace_policies.setdefault(api_id, [])
+        api_dict_update_replace.append(update_replace_policy)
