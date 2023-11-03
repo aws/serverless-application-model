49a4878276533fc15c4de0456f1fe8772d8f2e10
Xia Zhao <xazhao@amazon.com>
2023-11-03 10:48:52 -0700

    chore: replace nodejs14.x with nodejs18.x

--- bin/run_cfn_lint.sh
+++ bin/run_cfn_lint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 
-VENV=.venv_cfn_lint
+VENV=.venv
 
 # Install to separate venv to avoid circular dependency; cfn-lint depends on samtranslator
 # See https://github.com/aws/serverless-application-model/issues/1042
