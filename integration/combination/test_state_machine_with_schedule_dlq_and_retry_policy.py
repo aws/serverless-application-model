381cd7193f3e9e0046efeaa03e1da332c0a24e66
_sam <3804518+aahung@users.noreply.github.com>
2023-11-25 18:01:07 -0800

    chore: Upgrade ruff to 0.1.*

--- integration/combination/test_state_machine_with_schedule_dlq_and_retry_policy.py
+++ integration/combination/test_state_machine_with_schedule_dlq_and_retry_policy.py
@@ -46,7 +46,7 @@ class TestStateMachineWithScheduleDlqAndRetryPolicy(BaseTest):
 
         start_execution_policy_statement = start_execution_policy[0]
 
-        self.assertTrue(type(start_execution_policy_statement["Action"]) != list)
+        self.assertFalse(isinstance(start_execution_policy_statement["Action"], list))
         policy_action = start_execution_policy_statement["Action"]
         self.assertEqual(
             policy_action,
@@ -54,7 +54,7 @@ class TestStateMachineWithScheduleDlqAndRetryPolicy(BaseTest):
             "Action referenced in event role policy must be 'states:StartExecution'",
         )
 
-        self.assertTrue(type(start_execution_policy_statement["Resource"]) != list)
+        self.assertFalse(isinstance(start_execution_policy_statement["Resource"], list))
         referenced_state_machine_arn = start_execution_policy_statement["Resource"]
         self.assertEqual(
             referenced_state_machine_arn,
