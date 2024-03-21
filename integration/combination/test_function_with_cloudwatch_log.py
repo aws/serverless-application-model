455e4d9d713f0e20410e5ff5d1c9633c48ee67c2
Aayush Thapa <aaythapa@amazon.com>
2024-03-21 13:32:37 -0700

    add Logs service to skip certain tests

--- integration/combination/test_function_with_cloudwatch_log.py
+++ integration/combination/test_function_with_cloudwatch_log.py
@@ -1,6 +1,17 @@
+import logging
+from unittest.case import skipIf
+
 from integration.helpers.base_test import BaseTest
+from integration.helpers.resource import current_region_does_not_support
+from integration.config.service_names import LOGS
 
 
+LOG = logging.getLogger(__name__)
+
+
+@skipIf(
+    current_region_does_not_support([LOGS]), "Logs resource part of this test is not supported in this testing region"
+)
 class TestFunctionWithCloudWatchLog(BaseTest):
     def test_function_with_cloudwatch_log(self):
         self.create_and_verify_stack("combination/function_with_cloudwatch_log")
