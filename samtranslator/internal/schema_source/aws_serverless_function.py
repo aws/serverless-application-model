42afd7a3820a555469ebd6d06fe6ff779fca1b3e
Aayush Thapa <aaythapa@amazon.com>
2024-08-19 17:31:47 +0000

    feat: Support RecursiveLoop property in SAM function

--- samtranslator/internal/schema_source/aws_serverless_function.py
+++ samtranslator/internal/schema_source/aws_serverless_function.py
@@ -506,6 +506,7 @@ EphemeralStorage = Optional[PassThroughProp]
 SnapStart = Optional[PassThroughProp]  # TODO: check the type
 RuntimeManagementConfig = Optional[PassThroughProp]  # TODO: check the type
 LoggingConfig = Optional[PassThroughProp]  # TODO: add documentation
+RecursiveLoop = Optional[PassThroughProp]
 
 
 class Properties(BaseModel):
@@ -632,6 +633,7 @@ class Properties(BaseModel):
     VersionDescription: Optional[PassThroughProp] = prop("VersionDescription")
     VpcConfig: Optional[VpcConfig] = prop("VpcConfig")
     LoggingConfig: Optional[PassThroughProp]  # TODO: add documentation
+    RecursiveLoop: Optional[PassThroughProp]  # TODO: add documentation
 
 
 class Globals(BaseModel):
@@ -690,6 +692,7 @@ class Globals(BaseModel):
     SnapStart: Optional[SnapStart] = prop("SnapStart")
     RuntimeManagementConfig: Optional[RuntimeManagementConfig] = prop("RuntimeManagementConfig")
     LoggingConfig: Optional[PassThroughProp]  # TODO: add documentation
+    RecursiveLoop: Optional[PassThroughProp]  # TODO: add documentation
 
 
 class Resource(ResourceAttributes):
