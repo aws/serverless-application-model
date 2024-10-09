9e58cd0401f7bf438b55c9ee126c842e37863ffe
Aayush Thapa <aaythapa@amazon.com>
2024-10-09 17:40:56 -0400

    add to globals

--- samtranslator/plugins/globals/globals.py
+++ samtranslator/plugins/globals/globals.py
@@ -96,6 +96,7 @@ class Globals:
         ],
         SamResourceType.SimpleTable.value: ["SSESpecification"],
         SamResourceType.StateMachine.value: ["PropagateTags"],
+        SamResourceType.LambdaLayerVersion.value: ["PublishLambdaVersion"],
     }
     # unreleased_properties *must be* part of supported_properties too
     unreleased_properties: Dict[str, List[str]] = {
