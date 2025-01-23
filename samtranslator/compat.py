e56fc0a7054d78373ce599585c8b3c3e02261888
Roger Zhang <ruojiazh@amazon.com>
2025-01-23 00:06:13 +0000

    format

--- samtranslator/compat.py
+++ samtranslator/compat.py
@@ -1,8 +1,9 @@
 try:
     from pydantic import v1 as pydantic
+
     # Starting Pydantic v1.10.17, pydantic import v1 will success,
     # adding the following line to make Pydantic v1 should fall back to v1 import correctly.
-    pydantic.error_wrappers
+    pydantic.error_wrappers.ValidationError  # noqa
 except ImportError:
     # Unfortunately mypy cannot handle this try/expect pattern, and "type: ignore"
     # is the simplest work-around. See: https://github.com/python/mypy/issues/1153
