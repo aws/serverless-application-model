dd77e73a53fb58ef3de68a77661c36990027ea5b
Roger Zhang <ruojiazh@amazon.com>
2025-10-08 13:48:51 -0700

    lint

--- samtranslator/intrinsics/actions.py
+++ samtranslator/intrinsics/actions.py
@@ -7,7 +7,7 @@ from samtranslator.model.exceptions import InvalidDocumentException, InvalidTemp
 
 def _get_parameter_value(parameters: Dict[str, Any], param_name: str, default: Any = None) -> Any:
     """
-    Get parameter value from parameters dict, but return default (None) if 
+    Get parameter value from parameters dict, but return default (None) if
     - it's a CloudFormation internal placeholder.
     - param_name is not in the parameters.
 
